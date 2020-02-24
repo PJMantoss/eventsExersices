a13de23e360a24d7bb4efc20003a2ebc47682d44
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-24 13:15:07 +0100

    update script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-//1 Add the necessary code to wait for the DOM to load to make sure that anything you manipulate in the DOM has loaded. 
-let change = document.getElementById("change_heading");
-change.innerHTML = "Hello World!";
+//Part 1
+//Q.1 Add the necessary code to wait for the DOM to load to make sure that anything you manipulate in the DOM has loaded. 
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM fully loaded and parsed");
+  });
