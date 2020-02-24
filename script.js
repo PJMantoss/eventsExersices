aa110eab4be44097207381639049c489ba699a24
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-24 11:31:04 +0100

    update script

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-
+//1 Add the necessary code to wait for the DOM to load to make sure that anything you manipulate in the DOM has loaded. 
 let change = document.getElementById("change_heading");
 change.innerHTML = "Hello World!";
